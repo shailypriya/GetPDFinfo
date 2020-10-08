c3cb2841f0b62a99ebc5349fa7c889d74aec10b6
shailypriya <spshailypriyasingh@gmail.com>
2020-10-09 03:10:06 +0530

    First Commit

---
+++ GetPDFinfo.py
@@ -0,0 +1,25 @@
+from PyPDF2 import PdfFileReader
+
+def extract_information(pdf_path):
+    with open(pdf_path, 'rb') as f:
+        pdf = PdfFileReader(f)
+        information = pdf.getDocumentInfo()
+        number_of_pages = pdf.getNumPages()
+
+    txt = f"""
+    Information about {pdf_path}: 
+
+    Author: {information.author}
+    Creator: {information.creator}
+    Producer: {information.producer}
+    Subject: {information.subject}
+    Title: {information.title}
+    Number of pages: {number_of_pages}
+    """
+
+    print(txt)
+    return information
+
+if __name__ == '__main__':
+    path = 'C:\\Users\\Shaily Priya\\Desktop\\projects\\txt_from_pdf\\Think Python - Downey book.pdf'
+    extract_information(path)
